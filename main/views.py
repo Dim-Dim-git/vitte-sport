247234e8850bd21b5b6752a25aee939ccc4f8568
Dim-Dim-git <121255316+Dim-Dim-git@users.noreply.github.com>
2026-05-27 22:15:49 +0300

    Moved News to base.html, testing template Add comments

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from .models import Sport, News, UserProfile, Training
 
+# Главная страница
 def index(request):
     sports = Sport.objects.all()
     news   = News.objects.all()
@@ -12,10 +13,12 @@ def index(request):
         'news':   news,
     })
 
+# Выход из системы 
 def logout_view(request):
     logout(request)
     return redirect('/')
 
+# Регистрация нового пользователя
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -27,12 +30,14 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
+# Личный кабинет (доступен только авторизованным пользователям)
 @login_required
 def profile(request):
     return render(request, 'profile/index.html', {
         'user': request.user
     })
-    
+
+# Страница расписания    
 def schedule(request):
     trainings = Training.objects.all()
     days = Training.DAYS
@@ -40,13 +45,15 @@ def schedule(request):
         'trainings': trainings,
         'days': days,
     })
-    
+
+# Страница секци    
 def sports(request):
     sports = Sport.objects.all()
     return render(request, 'sports.html', {
         'sports': sports
     })
-    
+
+# Страница новостей    
 def news(request):
     news = News.objects.all()
     return render(request, 'news.html', {
