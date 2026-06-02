0cbe9e05c2cca07ab3322be213d940b1d56f43f6
Dim-Dim-git <121255316+Dim-Dim-git@users.noreply.github.com>
2026-06-02 19:50:12 +0300

    Add custom admin actions and filters for Feedback ans TrainingRecord

--- main/admin.py
+++ main/admin.py
@@ -1,13 +1,37 @@
 from django.contrib import admin
-from .models import Sport, News, UserProfile, Training, Feedback, TrainingRecord, Gallery, Tournament, TournamentParticipant
+from .models import Sport, News, UserProfile, Training, Feedback, TrainingRecord, Gallery, Tournament, TournamentParticipant, Achievement
 
 
 admin.site.register(Sport)
 admin.site.register(News)
 admin.site.register(UserProfile)
 admin.site.register(Training)
-admin.site.register(Feedback)
-admin.site.register(TrainingRecord)
+
+# Отметить обращения как прочитанные
+def mark_as_read(modeladmin, request, queryset):
+    queryset.update(is_read=True)
+mark_as_read.short_description = 'Отметить как прочитанное'
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display  = ('name', 'email', 'created', 'is_read')
+    list_filter   = ('is_read',)
+    search_fields = ('name', 'email')
+    actions       = [mark_as_read]
+
+# Отметить явку студентов
+def mark_attended(modeladmin, request, queryset):
+    queryset.update(attended=True)
+mark_attended.short_description = 'Отметить явку'
+
+@admin.register(TrainingRecord)
+class TrainingRecordAdmin(admin.ModelAdmin):
+    list_display  = ('user', 'training', 'created', 'attended')
+    list_filter   = ('attended', 'training__sport')
+    search_fields = ('user__username',)
+    actions       = [mark_attended]
+
 admin.site.register(Gallery)
 admin.site.register(Tournament)
 admin.site.register(TournamentParticipant)
+admin.site.register(Achievement)
